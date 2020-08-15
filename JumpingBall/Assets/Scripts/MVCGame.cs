61587e13737452c6639d331d5879d2bf72265707
WhatHaveIDone <37175348+WhatHaveIDone@users.noreply.github.com>
2020-08-15 17:26:20 +0800

    0815

---
+++ JumpingBall/Assets/Scripts/MVCGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MVCGame : MonoBehaviour
+{
+    [SerializeField] GameObject mod;
+    void Start()    
+    {
+        // 初始化時執行一次
+        Debug.Log("初始化完成~");
+        
+        // 實例化模組
+        Instantiate(mod);
+    }
+
+    void Update()  
+    {
+        // 每一禎執行一次
+    }
+}
